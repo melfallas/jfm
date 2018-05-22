66115fae2eb0336cda56c24c3b0ddbfe1265d729
Andres Hernandez <andres11889@gmail.com>
2018-05-22 00:10:52 -0600

    Adding Login servlet

---
+++ src/com/journaldev/servlet/LoginServlet.java
@@ -0,0 +1,124 @@
+package com.journaldev.servlet;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    public static Connection connObj;
+
+    //Datasys
+//    public static String JDBC_URL = "jdbc:sqlserver://172.31.251.128:1433;databaseName=compliancedb";
+//    public static String user = "compliancelogin";
+//    public static String pass = "Datasys123";
+//    public static String table = "[compliancedb].[dbo].[jc_users]";
+
+    //Guatemala
+    public static String JDBC_URL = "jdbc:sqlserver://172.18.142.15:1433;databaseName=compliance";
+    public static String user = "sa";
+    public static String pass = "Datasys123";
+    public static String table = "[compliance].[dbo].[jc_users]";
+
+    @Override
+    public void init() throws ServletException {
+        //Initialize Servlet
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String system = request.getParameter("system");
+        String sysUsername = request.getParameter("username");
+        System.out.println(system);
+        System.out.println(sysUsername);
+        JSONObject obj = new JSONObject();
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connObj = DriverManager.getConnection(JDBC_URL,user,pass);
+            if(connObj != null) {
+                Statement statement = connObj.createStatement();
+                ResultSet results = statement.executeQuery("SELECT  username, password " +
+                        "FROM " + table +
+                        " WHERE system = '"+ system + "' AND sys_username = '" + sysUsername + "'");
+
+                while(results.next()) {
+                    obj = new JSONObject();
+                    obj.put("username", results.getString("username"));
+                    obj.put("password", results.getString("password"));
+                }
+            }
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        }
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        System.out.println(obj);
+
+        out.print(obj);
+        out.flush();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String systemName = request.getParameter("system");
+        String systemUsername = request.getParameter("username");
+        String username = request.getParameter("jusername");
+        String password = request.getParameter("jpassword");
+        System.out.println(systemName);
+        System.out.println(systemUsername);
+        System.out.println(username);
+        System.out.println(password);
+        JSONObject obj = null;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connObj = DriverManager.getConnection(JDBC_URL,user,pass);
+            if(connObj != null) {
+                Statement statement = connObj.createStatement();
+                ResultSet results = statement.executeQuery("SELECT  username, password " +
+                        "FROM " + table +
+                        " WHERE system = '"+ systemName + "' AND sys_username = '" + systemUsername + "'");
+
+                while(results.next()) {
+                    obj = new JSONObject();
+                    obj.put("username", results.getString("username"));
+                    obj.put("password", results.getString("password"));
+                }
+
+                if(obj == null){
+                    PreparedStatement stmt = connObj.prepareStatement("INSERT INTO " + table + " (system, sys_username, username, password) VALUES (?, ?, ?, ?)");
+                    stmt.setString(1, systemName);
+                    stmt.setString(2, systemUsername);
+                    stmt.setString(3, username);
+                    stmt.setString(4, password);
+                    stmt.executeUpdate();
+                }else{
+                    PreparedStatement stmt = connObj.prepareStatement("UPDATE " + table + " SET username = ?, password = ? WHERE system = ? AND sys_username = ?" );
+                    stmt.setString(1, username);
+                    stmt.setString(2, password);
+                    stmt.setString(3, systemName);
+                    stmt.setString(4, systemUsername);
+                    stmt.executeUpdate();
+                }
+            }
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        }
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        System.out.println(obj);
+
+        out.print(obj);
+        out.flush();
+    }
+
+}
