39a10fa414a813b465f08b790b7d63faeb5cd716
Melvin <melvin.fallas@datasys.la>
2018-09-24 16:44:09 -0600

    E. 2018. JabberFileManager. Constants reorder changes.
    E. 2018. JabberFileManager. Constants reorder changes.

--- src/com/journaldev/servlet/CommonConstants.java
+++ src/com/journaldev/servlet/CommonConstants.java
@@ -4,8 +4,13 @@ public class CommonConstants {
 	
 	// Constantes de los ambientes local y producción
 	
-    // Local - Datasys
-	public static String JDBC_URL = "jdbc:sqlserver://172.31.251.128:1433;databaseName=compliancedb";
+	//--------------------------- Constantes para Ambiente de Datasys -------------------------//
+  	// Constantes Servidores
+	public static String BD_SERVER = "172.31.251.128:1433";
+	public static String AD_SERVER = "http://172.31.251.128:8085/";
+  	public static String FILE_SERVER = "http://172.31.251.11:8080/";
+  	// Parámetros Generales
+	public static String JDBC_URL = "jdbc:sqlserver://" + BD_SERVER + ";databaseName=compliancedb";
 	public static String DB_USER = "compliancelogin";
 	public static String DB_PASSWORD = "Datasys123";
  	public static String LOGIN_TABLE = "[compliancedb].[dbo].[jc_users]";
@@ -15,14 +20,20 @@ public class CommonConstants {
   	public static String ADMIN_CHATROOMS_TABLE = "[compliancedb].[dbo].[tc_rooms]";
 	public static String ROOT_PATH = "/home/soporte/fileserver";
 	public static String FOLDER_PATH = "/home/soporte/fileserver/files/";
-	public static String FILE_SERVER_DOWNLOAD = "http://172.31.251.11:8080/JabberFileManager/GetFile?filename=";
-//  	public static String urlADcredentialService = "http://172.31.251.128:8085/api/UsuarioADObtenerPorCredenciales/"+username+"/"+password+"/";
-//  	public static String userAuthenticationServiceURL = "http://172.31.251.11:8080/JabberFileManager/UserAuthenticationServlet?webuser="+username+"&pass="+password+"&filename="+encoded+"";
-//  	public static String urlUserLogin = "http://172.31.251.11:8080/JabberFileManager/LoginServlet?system=temp&username=temp&jusername="+$("#username").val()+"&jpassword="+$("#password").val()+"";
-
-  	
-	// Produccion - Guatemala
-//	public static String JDBC_URL = "jdbc:sqlserver://172.18.142.15:1433;databaseName=compliancedb";
+	public static String FILE_SERVER_DOWNLOAD = FILE_SERVER + "JabberFileManager/GetFile?filename=";
+	//Servicios para peticiones
+  	public static String AD_CREDENTIAL_SERVICE_URL = AD_SERVER + "api/UsuarioADObtenerPorCredenciales/";
+  	public static String USER_AUTHENTICATION_SERVICE_URL = FILE_SERVER + "JabberFileManager/UserAuthenticationServlet";
+  	public static String USER_LOGIN_SERVICE_URL = FILE_SERVER + "JabberFileManager/LoginServlet";
+	
+	
+	//--------------------------- Constantes para Ambiente de Guatemala -------------------------//
+//  	// Constantes Servidores
+//	public static String BD_SERVER = "172.18.142.15:1433";
+//	public static String AD_SERVER = "http://172.18.142.15:8085/";
+//  	public static String FILE_SERVER = "http://mp-fsapp01.mp.gob.gt:8080/";
+//	// Parámetros Generales
+//	public static String JDBC_URL = "jdbc:sqlserver://" + BD_SERVER + ";databaseName=compliancedb";
 //	public static String DB_USER = "datasysUser";
 //	public static String DB_PASSWORD = "Datasys123";
 //	public static String LOGIN_TABLE = "compliancedb.jabber.jc_users";
@@ -32,16 +43,15 @@ public class CommonConstants {
 //	public static String ADMIN_CHATROOMS_TABLE = "[compliancedb].[dbo].[tc_rooms]";
 //	public static String ROOT_PATH = "/fileserver/archivos/server1";
 //	public static String FOLDER_PATH = "/fileserver/archivos/server1/files/";
-//  	public static String FILE_SERVER_DOWNLOAD = "http://mp-fsapp01.mp.gob.gt:8080/JabberFileManager/GetFile?filename=";	
-//
-//	//Servicios Guatemala
-//  	public static String AD_CREDENTIAL_SERVICE_URL = "http://172.18.142.15:8085/api/UsuarioADObtenerPorCredenciales/";
-//  	public static String USER_AUTHENTICATION_SERVICE_URL = "http://mp-fsapp01.mp.gob.gt:8080/JabberFileManager/UserAuthenticationServlet";
-//  	public static String USER_LOGIN_SERVICE_URL = "http://mp-fsapp01.mp.gob.gt:8080/JabberFileManager/LoginServlet";
+//  	public static String FILE_SERVER_DOWNLOAD = FILE_SERVER + "JabberFileManager/GetFile?filename=";	
+//  	//Servicios para peticiones
+//  	public static String AD_CREDENTIAL_SERVICE_URL = AD_SERVER + "api/UsuarioADObtenerPorCredenciales/";
+//  	public static String USER_AUTHENTICATION_SERVICE_URL = FILE_SERVER + "JabberFileManager/UserAuthenticationServlet";
+//  	public static String USER_LOGIN_SERVICE_URL = FILE_SERVER + "JabberFileManager/LoginServlet";
 
 	
 	
-	// Comunes
+	// Constantes Comunes
 	public static String JWU_GetWebChatUser = "[jabber].[SP_JWU_GetWebChatUser]";
     public static String JWU_SaveWebChatUser = "[jabber].[SP_JWU_SaveWebChatUser]";
     public static String SP_JWU_CountWebChatUserRegister = "[jabber].[SP_JWU_CountWebChatUserRegister]";
